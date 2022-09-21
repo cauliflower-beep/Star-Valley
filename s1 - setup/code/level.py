6df0f90dcd7661ee1d619104665ed91b767909fa
lsx <642545320@qq.com>
2022-09-21 15:34:50 +0800

    初始化窗口

--- s1 - setup/code/level.py
+++ s1 - setup/code/level.py
@@ -1,16 +1,17 @@
-import pygame 
+import pygame
 from settings import *
 
+
 class Level:
-	def __init__(self):
+    def __init__(self):
+        # get the display surface
+        # 得到的结果和main中，Game.screen 显示界面是同一个
+        self.display_surface = pygame.display.get_surface()
 
-		# get the display surface
-		self.display_surface = pygame.display.get_surface()
+        # sprite groups角色组
+        self.all_sprites = pygame.sprite.Group()
 
-		# sprite groups
-		self.all_sprites = pygame.sprite.Group()
-
-	def run(self,dt):
-		self.display_surface.fill('black')
-		self.all_sprites.draw(self.display_surface)
-		self.all_sprites.update()
+    def run(self, dt):
+        self.display_surface.fill('black')
+        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.update()
--- s1 - setup/code/main.py
+++ s1 - setup/code/main.py
@@ -2,25 +2,31 @@ import pygame, sys
 from settings import *
 from level import Level
 
-class Game:
-	def __init__(self):
-		pygame.init()
-		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-		pygame.display.set_caption('Sprout land')
-		self.clock = pygame.time.Clock()
-		self.level = Level()
 
-	def run(self):
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
-  
-			dt = self.clock.tick() / 1000
-			self.level.run(dt)
-			pygame.display.update()
+# 游戏类
+class Game:
+    def __init__(self):
+        # https://blog.csdn.net/qingtianZzzzz/article/details/84027873
+        # init动作就是初始化所有模块，看看pygame是否可以正常为我们提供帮助
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Star Valley')
+        self.clock = pygame.time.Clock()
+        # 为了保持主函数尽可能简洁，这里level单独做一个模块。
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000
+            self.level.run(dt)
+            pygame.display.update()
+
 
 if __name__ == '__main__':
-	game = Game()
-	game.run()
+    game = Game()
+    game.run()
